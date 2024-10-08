fb14b3959c28cce1bb1ad515abac75c821904b5c
Leonid Kuzmychenko <lleonidk@ukr.net>
2024-10-09 00:25:19 +0300

    first commit

---
+++ src/test/java/com/example/githubenv/GithubEnvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubenv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubEnvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
